5d411f80d9d3391657d8fd95e04457581b619f88
mcanas <mcanas@users.noreply.github.com>
2026-06-29 04:08:33 +0000

    deploy: 046194e17625a5c9d58a26819ab7c3521d9b5d1d

--- assets/swimlanesDiagram-G3AALYLV-zVCi_8cs.js
+++
@@ -1,8 +0,0 @@
-import{n as e}from"./chunk-Y2CYZVJY-DsF7k-Jl.js";import"./src-DG-gtITI.js";import"./chunk-WYO6CB5R-CM4r31Dy.js";import"./chunk-ICXQ74PX-BjGBXNZw.js";import"./chunk-HOUHSVGY-CYpFaKzS.js";import"./chunk-Q4XR5HBZ-d9cD13II.js";import"./chunk-7BUUIJ7U-Bb538aSH.js";import"./chunk-OGEWGWER-B-glAxDm.js";import"./chunk-32BRIVSS-L4EfqTXX.js";import"./chunk-XXDRQBXY-1Rp8gyAJ.js";import"./chunk-VR4S4FIN-GLMXbE67.js";import"./chunk-C7G6YPKG-DX2m_YE7.js";import"./chunk-ZGVPDNZ5-CQ0zRXUb.js";import"./chunk-52WLFC77-Cu6hcJSL.js";import"./chunk-FWX5IMBZ-DTcAoEWL.js";import"./chunk-ZIRB5QZD-C6fEPe3t.js";import{r as t,t as n}from"./chunk-PUDLZKDR-CunbWLKE.js";var r=n({defaultLayout:`swimlane`,styles:e(e=>`${t(e)}
-  .swimlane.cluster rect {
-    stroke: ${e.clusterBorder} !important;
-  }
-  [data-look="neo"].cluster rect {
-    filter: none;
-  }
-`,`getStyles`)});export{r as diagram};
